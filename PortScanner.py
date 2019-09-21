0b285f1609c0c540e3c94465cba0a692bff8da75
Vishrant Khanna <vishrant97@gmail.com>
2019-09-21 15:18:33 +0530

    Update PortScanner.py
    I updated the python 3 script to automatically scan for popularly used ports by servers.

--- PortScanner.py
+++ PortScanner.py
@@ -2,17 +2,78 @@
 
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
+ip = input("IP Address: ")
 
-host = input("Please enter the IP you want to scan: ")
-port = int(input("Please enter the port you want to scan: "))
+port_list = {
+    1: "TCP Port Service Multiplexer (TCPMUX)",
+    5: "Remote Job Entry (RJE)",
+    7: "ECHO",
+    18: "Message Send Protocol (MSP)",
+    20: "FTP -- Data",
+    21: "FTP -- Control",
+    22: "SSH Remote Login Protocol",
+    23: "Telnet",
+    25: "Simple Mail Transfer Protocol (SMTP)",
+    29: "MSG ICP",
+    37: "Time",
+    42: "Host Name Server (Nameserv)",
+    43: "WhoIs",
+    49: "Login Host Protocol (Login)",
+    53: "Domain Name System (DNS)",
+    69: "Trivial File Transfer Protocol (TFTP)",
+    70: "Gopher Services",
+    79: "Finger",
+    80: "HTTP",
+    103: "X.400 Standard",
+    108: "SNA Gateway Access Server",
+    109: "POP2",
+    110: "POP3",
+    115: "Simple File Transfer Protocol (SFTP)",
+    118: "SQL Services",
+    119: "Newsgroup (NNTP)",
+    137: "NetBIOS Name Service",
+    139: "NetBIOS Datagram Service",
+    143: "Interim Mail Access Protocol (IMAP)",
+    150: "NetBIOS Session Service",
+    156: "SQL Server",
+    161: "SNMP",
+    179: "Border Gateway Protocol (BGP)",
+    190: "Gateway Access Control Protocol (GACP)",
+    194: "Internet Relay Chat (IRC)",
+    197: "Directory Location Service (DLS)",
+    389: "Lightweight Directory Access Protocol (LDAP)",
+    396: "Novell Netware over IP",
+    443: "HTTPS",
+    444: "Simple Network Paging Protocol (SNPP)",
+    445: "Microsoft-DS",
+    458: "Apple QuickTime",
+    546: "DHCP Client",
+    547: "DHCP Server",
+    563: "SNEWS",
+    569: "MSN",
+    1080: "Socks",
+}
+
+print("\n")
+
+print("List of commonly used ports: ")
+for port, name in port_list.items():
+    print(port, name)
+
+print("\n")
 
 
-def portScanner(port):
-    if s.connect_ex((host, port)):
-        print("The port is closed")
+def scanner(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(10)
+
+    if s.connect_ex((ip, port)):
+        print(port, "is closed.")
     else:
-        print("The port is open")
+        print(port, "is open.")
 
-portScanner(port)
+    s.close()
+
+
+for port in port_list:
+    scanner(ip, port)
